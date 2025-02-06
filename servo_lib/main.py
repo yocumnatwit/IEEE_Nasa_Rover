c044ea87c96f4b00a06c908fbc4fa0cb4bbc817b
Noah Yocum <yocumn@wit.edu>
2025-02-06 13:51:39 -0500

    Add some extra functions for QOL

--- servo_lib/main.py
+++ servo_lib/main.py
@@ -8,8 +8,29 @@ from adafruit_pca9685 import PCA9685
 
 class Servo_Lib:
     
-    def __init__(self, servoObj):
-        self.servo = servoObj
+    def __init__(self, servoObj=None):
+        if servoObj is not None:
+            self.servo = servoObj
+            self.pca = None
+        else:
+            print("Warning: no servo obj provided!")
+            print("Please use init_servo before proceeding!")
+            pass
+
+    def init_servo(self, channel, frequency=50):
+        """
+        Create a servo object on a channel
+        frequency: the frequency at which to operate the servo. Default: is 50
+        """
+        if channel > 7 or channel < 0:
+            raise ValueError("Invalid channel!")
+        if frequency <= 0 or frequency > 100:
+            raise ValueError("Invalid frequency!")
+        i2c = board.I2C()
+        self.pca = PCA9685(i2c)
+        self.pca.frequency = frequency
+        self.servo = servo.Servo(self.pca.channels[channel])
+
 
     def rotateTo(self, pos_deg, step_delay=0.03):
         """
@@ -37,6 +58,8 @@ class Servo_Lib:
             time.sleep(step_delay)
 
     def self_destruct(self):
+        if self.pca is not None:
+            self.pca.deinit()
         del self
 
     
