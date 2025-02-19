1396d71916b61215f8c2504191e1d7b2cbdcc4ff
Noah Yocum <yocumn@wit.edu>
2025-02-19 12:10:56 -0500

    Initialize controller values

--- components/controller_lib/setup.py
+++ components/controller_lib/setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="wit_ieee_nasa_rover_motor_lib",
+    name="wit_ieee_nasa_rover_controller_lib",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         'time',
-        'RPI.GPIO',
+        'threading',
+        'math',
+        'inputs',
     ],
     #test_suite='tests',
 )
