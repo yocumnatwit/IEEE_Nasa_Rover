804c3877f6e72a1f840f1e9972e85b5256393aaa
yocumnatwit <yocumn@wit.edu>
2025-03-19 15:07:00 +0000

    Attempt steering control version 1

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from components.servo_lib.slm import servo_lib as srv_lib
 
 import time
 
+def servoSetupSteering(channels):
+    for channel in channels:
+        servo = srv_lib.Servo_Lib()
+        servo.init_servo(0, 100)
+        servo.setPosition(60)
+
 if __name__ == '__main__':
     servo = srv_lib.Servo_Lib()
     controller = ctrl_lib.Controller()
+    servoSetupSteering()
     servo.init_servo(0, 100)
-    servo.setPosition(0)
-    time.sleep(0.25)
+    pressed = False
+    while not pressed:
+        buttons = controller.readLetterButtons()
+        if buttons[0] == 1:
+            pressed = True
+        sticks = controller.readSticks()
+        servo.servo.angle = servo.getAngle + sticks[0]
+
+    
